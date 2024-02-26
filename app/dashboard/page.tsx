4f04a20dd504c3f9c04efebe9789ac10c59595df
Bhuwan <bhubanpaudel79@gmail.com>
2024-02-26 17:28:36 +0545

    add use client in page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,11 @@
+'use client'
 import Input from "./Input"
 export default function MainPage() {
     return (
         <div className="main-body px-10 sm:px-32 py-10 text-center text-Text">
-            <h1 className=" text-3xl sm:text-4xl pb-10">
-                You can list tasks for your busy schedule</h1>
+            <h1 className=" text-3xl sm:text-4xl pb-10">You can list tasks for your busy schedule</h1>
             <div className="input">
-              <Input/>
+                <Input />
             </div>
         </div>
     )
